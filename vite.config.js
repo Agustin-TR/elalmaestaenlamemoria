eb842432d5f9105c55f5b085aa33a3fa2e39bbee
agustinrojas <rojas.agustint@gmail.com>
2025-11-13 15:49:09 -0300

    base update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
 
-  base: '/nombre-de-tu-repositorio/', 
+  base: '/elAlmaEstaEnLaMemoria/', 
 
   plugins: [
     vue(),
