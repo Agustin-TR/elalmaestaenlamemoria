edadcd3158d23cae7eadecb132f8529c41466f40
agustinrojas <rojas.agustint@gmail.com>
2026-01-19 13:11:15 -0300

    comentarios nuevos

--- src/data/comentarios.js
+++ src/data/comentarios.js
@@ -6,27 +6,57 @@ export const dataComentarios = [
     },
     {
         id: 2,
-        nombreUsuario: 'Martín L.',
-        comentario: 'La impresión es de una calidad excepcional. La forma en que se complementan las letras y las imágenes crea una experiencia inmersiva única. Un must-have.'
+        nombreUsuario: 'Lorenzo',
+        comentario: 'Todo lo hecho con el corazón sale bien. Este es el claro ejemplo del esfuerzo y la pasión combinados en pos de una hermosa obra. ¡Felicitaciones!'
     },
     {
         id: 3,
-        nombreUsuario: 'Sofía J.',
-        comentario: 'Leí "Mil canciones y un café" y me hizo sentir esa nostalgia dulce. Muy recomendable para quienes buscan un arte que les hable directamente al corazón.'
+        nombreUsuario: 'Candela',
+        comentario: 'Me encantó! Conecté con cada poema desde diferentes lugares, y todos me dejaron queriendo leer uno más'
     },
     {
         id: 4,
-        nombreUsuario: 'Javier P.',
-        comentario: 'Me encantó el detalle de los fotógrafos. Es un proyecto colaborativo muy bien logrado. Espero ver más obras así pronto. Cinco estrellas.'
+        nombreUsuario: 'Renata Fain',
+        comentario: 'Que privilegio tener tus poesías después de tanta espera y leerlas una y otra vez. Feliz de tus logros y de poder acompañarte! Sos enorme amigo'
     },
     {
         id: 5,
-        nombreUsuario: 'Andrea B.',
-        comentario: 'Simplemente hermoso. La composición visual y lírica es perfecta. Se lo regalé a una amiga y le fascinó. Es arte puro.'
+        nombreUsuario: 'Melissa',
+        comentario: 'Mucha admiración! La escritura como sanación y transformación, creaste una obra hermosa llena de emoción! Felicidades'
     },
     {
         id: 6,
-        nombreUsuario: 'Elena R.',
+        nombreUsuario: 'Nai',
+        comentario: 'Se nota mucho el cariño y el tiempo que le dedicaste. Cada poema y cada foto están ahí por algo. Hermoso trabajo te mandaste!'
+    },
+     {
+        id: 7,
+        nombreUsuario: 'Lucas',
+        comentario: 'Me sorprendió para bien: directo, emotivo y con imágenes muy potentes. Vale mucho la pena.'
+    },
+    {
+        id: 8,
+        nombreUsuario: 'Sofía',
+        comentario: 'Leí "Mil canciones y un café" y me hizo sentir esa nostalgia dulce. Muy recomendable para quienes buscan un arte que les hable directamente al corazón.'
+    },
+     {
+        id: 9,
+        nombreUsuario: 'Cami',
+        comentario: 'Me encantó el detalle de los fotógrafos. Es un proyecto colaborativo muy bien logrado. Espero ver más obras así pronto. Cinco estrellas.'
+    },
+     {
+        id: 10,
+        nombreUsuario: 'Florencia',
+        comentario: 'Simplemente hermoso. La composición visual y lírica es perfecta. Se lo regalé a una amiga y le fascinó. Es arte puro.'
+    },
+    {
+        id: 11,
+        nombreUsuario: 'Agos',
         comentario: 'Una colección que te toca el alma. "Escrituras efímeras infinitas" es mi favorito, la fusión de poesía y fotografía es sublime. ¡Felicidades al autor!'
+    },
+     {
+        id: 12,
+        nombreUsuario: 'Martín',
+        comentario: 'La forma en que se complementan las letras y las imágenes crea una experiencia inmersiva única. Un must-have.'
     },
 ];
